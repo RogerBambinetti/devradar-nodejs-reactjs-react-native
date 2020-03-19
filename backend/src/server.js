16a3cb2637d2ff7350a69caa76c6acc929458b6d
Roger Bambinetti <rogerbambinetti@hotmail.com>
2020-03-19 09:22:15 -0300

    backend started

---
+++ backend/src/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+const server = require('http').Server(app);;
+
+const routes = require('./routes');
+
+mongoose.connect('mongodb://rogerbambinetti:rogerbambinetti@cluster-shard-00-00-jmacf.mongodb.net:27017,cluster-shard-00-01-jmacf.mongodb.net:27017,cluster-shard-00-02-jmacf.mongodb.net:27017/devradar?ssl=true&replicaSet=Cluster-shard-0&authSource=admin&retryWrites=true&w=majority', { useNewUrlParser: true });
+
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+    return next();
+});
+
+app.use(express.json());
+app.use(cors());
+app.use(routes);
+
+server.listen(3333);
+
+
+app.use(express.json());
+app.use(cors());
+app.use(routes);
+
+server.listen(8080);
